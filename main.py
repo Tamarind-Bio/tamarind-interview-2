bedf381630f941befdc2a911d51474df7e507f64
Sherry Liu <sherryliu987@gmail.com>
2024-10-13 19:37:42 -0700

    populate queue

---
+++ main.py
@@ -0,0 +1,28 @@
+job_queue = [
+    {
+        "name":"1iep",
+        "status":"In Queue",
+        "settings":{
+            "receptor":"1iep_receptorH.pdbqt",
+            "ligand":"1iep_ligand.pdbqt",
+            "center":[15.190, 53.903, 16.917],
+            "size":[20, 20, 20],
+        }
+    }, {
+        "name":"1iep_whole",
+        "status":"In Queue",
+        "settings":{
+            "receptor":"1iep_receptorH.pdbqt",
+            "ligand":"1iep_ligand.pdbqt",
+            "whole":True
+        }
+    }, {
+        "name":"1iep_error",
+        "status":"In Queue",
+        "settings":{
+            "receptor":"1iep_receptorH.pdbqt",
+            "ligand":"1iep_ligand_error.pdbqt",
+            "whole":True
+        }
+    }
+]
